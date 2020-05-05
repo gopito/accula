0888175585e884ee7135c24f9312de0feec21f3c
VanyaKrylov <studentik7@yandex.ru>
2020-05-05 15:03:27 +0300

    Added util class

---
+++ ccd/src/main/java/org/accula/ccd/util/TokenSymbolicNameExtractor.java
@@ -0,0 +1,13 @@
+package org.accula.ccd.util;
+
+import org.accula.ccd.lexer.JavaLexer;
+import org.antlr.v4.runtime.Token;
+
+public class TokenSymbolicNameExtractor {
+
+    private TokenSymbolicNameExtractor() {}
+
+    public static String getSymbolicNameFrom(Token token) {
+        return JavaLexer.VOCABULARY.getSymbolicName(token.getType());
+    }
+}
