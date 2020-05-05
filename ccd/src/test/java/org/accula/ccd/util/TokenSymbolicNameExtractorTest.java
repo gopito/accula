1175c99593c5295529f66a8f55a9563c7ffcf5cf
VanyaKrylov <studentik7@yandex.ru>
2020-05-05 15:35:30 +0300

    Added unit test for TokenSymbolicNameExtractor

---
+++ ccd/src/test/java/org/accula/ccd/util/TokenSymbolicNameExtractorTest.java
@@ -0,0 +1,21 @@
+package org.accula.ccd.util;
+
+import org.accula.ccd.lexer.JavaLexer;
+import org.antlr.v4.runtime.Token;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class TokenSymbolicNameExtractorTest {
+
+    @Test
+    void getSymbolicNameFrom() {
+        Token token = mock(Token.class);
+        for (int tokenType = 1; tokenType <= 110; tokenType++) {
+            when(token.getType()).thenReturn(tokenType);
+            assertEquals(JavaLexer.VOCABULARY.getSymbolicName(tokenType), TokenSymbolicNameExtractor.getSymbolicNameFrom(token));
+        }
+    }
+}
